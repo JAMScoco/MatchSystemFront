ebce082da26375dde8ce5f71eb45c16fd062e23a
JAMScoco <xukungou@gmail.com>
2022-09-17 15:58:02 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>head</h1>
+    <router-view></router-view>
+    <p>foot</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    首页
+    <el-button @click="toNews">点击</el-button>
+  </div>
+</template>
+
+<script setup>
+
+import {useRouter} from "vue-router";
+
+const router = useRouter()
+
+
+function toNews() {
+  console.log(router)
+  router.push('/home/news')
+}
+</script>
+
+<style scoped>
+
+</style>
