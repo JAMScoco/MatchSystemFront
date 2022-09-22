3d7a139da21e14df475b9388a120851c6b1ac3d5
JAMScoco <xukungou@gmail.com>
2022-09-22 22:25:02 +0800

    主页样式修改 引入轮播图组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,63 +2,51 @@
   <div>
     <el-row class="head">
       <el-col :span="4"></el-col>
-      <el-col :span="16">
-        <div class="head_box">
-          <div style="display: flex;flex-direction: row">
-            <img src="../assets/logo/logo.png"/>
-            <span style="margin-top: 10px">全国大学生创业服务网</span>
-          </div>
-          <div>
-            <router-link to="#">首页</router-link>
-            <router-link to="#">赛事动态</router-link>
-            <router-link to="#">院系动态</router-link>
-          </div>
-          <div>
-            <router-link to="#">登录</router-link>
-            <router-link to="#">注册</router-link>
-          </div>
+      <el-col :span="4">
+        <div style="display: flex;flex-direction: row">
+          <img src="../assets/logo/logo.png"/>
+          <span style="margin-top: 20px">全国大学生创业服务网</span>
         </div>
       </el-col>
-      <el-col :span="4"></el-col>
+      <el-col :span="2">
+        <router-link to="/home/index"><h3 style="text-align: center">首页</h3></router-link>
+      </el-col>
+      <el-col :span="2">
+        <router-link to="/home/news"><h3>大赛动态</h3></router-link>
+      </el-col>
+      <el-col :span="2">
+        <router-link to="/home/news"><h3>学院动态</h3></router-link>
+      </el-col>
+      <el-col :span="7"></el-col>
+      <el-col :span="3" style="text-align: center;">
+        <p style="margin-top: 20px"><router-link to="/login">登录入口</router-link></p>
+      </el-col>
     </el-row>
     <router-view></router-view>
     <el-row class="foot">
-      <el-col :span="4"></el-col>
-      <el-col :span="16">
-        <div class="foot_box">
-          <div>
-            <img src="../assets/logo/logo.png"/>
-          </div>
-          <div>
-            <div style="display: flex;flex-direction: row">
-              <a href="#">国家24365大学生就业服务平台 </a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-              <a href="#">联系我们 </a>
-            </div>
-            <div>
-              <p>版权所有：教育部学生服务与素质发展中心（原全国高等学校学生信息咨询与就业指导中心）</p>
-            </div>
-          </div>
-          <div style="display: flex;flex-direction: row">
-            <div>
-              <div>
-                <img src="../assets/icons/svg/cascader.svg" alt="大创网官方微信平台">
-              </div>
-              <div>
-                <span>大创网官方微信平台</span>
-              </div>
-            </div>
-            <div>
-              <div>
-                <img src="../assets/icons/svg/cascader.svg" alt="大赛官方微信号">
-              </div>
-              <div>
-                <span>大赛官方微信号</span>
-              </div>
-            </div>
-          </div>
+      <el-col :span="5"></el-col>
+      <el-col :span="2">
+        <img src="../assets/images/profile.jpg" style="height: 70px;width: 70px;margin-top: 40px"/>
+      </el-col>
+      <el-col :span="7">
+        <div style="margin-top: 35px;">
+          <a href="#">国家24365大学生就业服务平台</a>|
+          <a href="#">长江大学 </a>|
+          <a href="#">联系我们 </a>
+          <p style="font-size: small">版权所有：教育部学生服务与素质发展中心（原全国高等学校学生信息咨询与就业指导中心）</p>
+          <p style="font-size: small">京ICP备xxxxxxx号-1京公网安备xxxxxxxxxxxxxx号</p>
+
         </div>
       </el-col>
-      <el-col :span="4"></el-col>
+      <el-col :span="2" style="text-align: center">
+        <img src="../assets/images/profile.jpg" style="height: 80px;width: 80px;margin-top: 25px"/>
+        <p style="font-size: small;margin-top: 2px">大赛官方微信平台</p>
+      </el-col>
+      <el-col :span="2" style="text-align: center">
+        <img src="../assets/images/profile.jpg" style="height: 80px;width: 80px;margin-top: 25px"/>
+        <p style="font-size: small;margin-top: 2px">大赛官方微信号</p>
+      </el-col>
+      <el-col :span="5"></el-col>
     </el-row>
   </div>
 </template>
@@ -70,23 +58,14 @@
 <style scoped>
 
 .head {
-  height: 50px;
+  height: 64px;
   background-color: #eef1f6;
-}
-
-.head_box {
-  display: flex;
-  flex-direction: row;
 }
 
 .foot {
   height: 150px;
-  background-color: #909399;
-}
-
-.foot_box {
-  display: flex;
-  flex-direction: row;
+  background-color: #312c2c;
+  color: white;
 }
 
 </style>
