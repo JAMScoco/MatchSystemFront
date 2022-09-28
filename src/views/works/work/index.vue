5ca4d2e153aa76966f1c0d2e26de517329f3d9b4
JAMScoco <xukungou@gmail.com>
2022-09-28 19:24:41 +0800

    学生查看作品检索完善

--- src/views/works/work/index.vue
+++ src/views/works/work/index.vue
@@ -1,21 +1,63 @@
 <template>
   <div class="app-container">
     <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
-      <el-form-item label=" 作品名称" prop="name">
+      <el-form-item label="作品名称" prop="name">
         <el-input
-          v-model="queryParams.name"
-          placeholder="请输入 作品名称"
-          clearable
-          @keyup.enter="handleQuery"
+            v-model="queryParams.name"
+            placeholder="请输入作品名称"
+            clearable
+            @keyup.enter="handleQuery"
         />
       </el-form-item>
       <el-form-item label="作品状态" prop="state">
-        <el-input
-          v-model="queryParams.state"
-          placeholder="请输入作品状态"
-          clearable
-          @keyup.enter="handleQuery"
-        />
+        <el-select v-model="queryParams.state" placeholder="请选择作品状态">
+          <el-option
+              v-for="dict in works_status"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="赛道" prop="trackId">
+        <el-select v-model="queryParams.trackId" placeholder="请选择赛道" @change="trackChange">
+          <el-option
+              v-for="item in optionsTrack"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="组别" prop="groupId">
+        <el-select v-model="queryParams.groupId" placeholder="请选择组别">
+          <el-option
+              v-for="item in optionsGroup"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="类别" prop="categoryId">
+        <el-select v-model="queryParams.categoryId" placeholder="请选择类别">
+          <el-option
+              v-for="item in optionsCategory"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item v-hasRole="['school']" label="院系" prop="deptId">
+        <el-select v-model="queryParams.deptId" placeholder="请选择院系">
+          <el-option
+              v-for="item in departments"
+              :key="item.deptId"
+              :label="item.deptName"
+              :value="item.deptId"
+          />
+        </el-select>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
@@ -26,49 +68,59 @@
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
         <el-button
-          type="primary"
-          plain
-          icon="Download"
-          @click="handleExport"
-          v-hasPermi="['works:work:export']"
-        >导出到Excel</el-button>
+            type="primary"
+            plain
+            icon="Download"
+            @click="handleExport"
+            v-hasPermi="['works:work:export']"
+        >导出到Excel
+        </el-button>
       </el-col>
       <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
     <el-table v-loading="loading" :data="workList">
-      <el-table-column type="index" label="序号" width="55" align="center" />
-      <el-table-column label="作品名称" align="center" prop="name" />
-      <el-table-column label="所属赛道" align="center" prop="trackId" />
-      <el-table-column label="所属组别" align="center" prop="groupId" />
-      <el-table-column label="所属类别" align="center" prop="categoryId" />
-      <el-table-column label="作品状态" align="center" prop="state" />
+      <el-table-column type="index" label="序号" width="55" align="center"/>
+      <el-table-column label="作品名称" align="center" prop="name"/>
+      <el-table-column label="所属院系" align="center" prop="deptName" v-hasRole="['school']"/>
+      <el-table-column label="负责人" align="center" prop="trueName"/>
+      <el-table-column label="所属赛道" align="center" prop="trackName"/>
+      <el-table-column label="所属组别" align="center" prop="groupName"/>
+      <el-table-column label="所属类别" align="center" prop="categoryName"/>
+      <el-table-column label="作品状态" align="center" prop="state">
+        <template #default="scope">
+          <dict-tag :options="works_status" :value="scope.row.state"/>
+        </template>
+      </el-table-column>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template #default="scope">
           <el-button
               type="success"
               icon="Warning"
-            @click="worksInfo(scope.row)"
-            v-hasPermi="['works:work:query']"
-          >详情</el-button>
+              @click="worksInfo(scope.row)"
+              v-hasPermi="['works:work:query']"
+          >详情
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
 
     <pagination
-      v-show="total>0"
-      :total="total"
-      v-model:page="queryParams.pageNum"
-      v-model:limit="queryParams.pageSize"
-      @pagination="getList"
+        v-show="total>0"
+        :total="total"
+        v-model:page="queryParams.pageNum"
+        v-model:limit="queryParams.pageSize"
+        @pagination="getList"
     />
   </div>
 </template>
 
 <script setup name="Work">
-import { listWork } from "@/api/works/work";
+import {listWork} from "@/api/works/work";
+import useTracks from "@/hooks/useTracks";
+import {getSchoolDepts} from "@/api/system/dept";
 
-const { proxy } = getCurrentInstance();
+const {proxy} = getCurrentInstance();
 
 const workList = ref([]);
 const open = ref(false);
@@ -89,10 +141,11 @@ const data = reactive({
     groupId: null,
     categoryId: null,
     state: null,
+    deptId: null
   },
 });
 
-const { queryParams} = toRefs(data);
+const {queryParams} = toRefs(data);
 
 /** 查询作品列表 */
 function getList() {
@@ -121,6 +174,8 @@ function worksInfo(row) {
   console.log(row)
 }
 
+const {works_status} = proxy.useDict('works_status')
+
 
 /** 导出按钮操作 */
 function handleExport() {
@@ -128,6 +183,21 @@ function handleExport() {
     ...queryParams.value
   }, `work_${new Date().getTime()}.xlsx`)
 }
+
+//赛道组别类别关联下拉框
+const {
+  trackChange,
+  optionsTrack,
+  optionsGroup,
+  optionsCategory
+} = useTracks
+
+//选择院系下拉框相关代码
+const departments = ref([])
+
+getSchoolDepts().then(res => {
+  departments.value = res.data
+})
 
 getList();
 </script>
