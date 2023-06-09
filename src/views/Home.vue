1edef67045d8abd1b60b047b23d6a28bc0996121
JAMScoco <xukungou@gmail.com>
2023-06-09 16:34:35 +0800

    登录判定首页显示内容

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,15 +16,17 @@
       <el-col :span="2">
         <router-link to="/home/news"><h2>学院动态</h2></router-link>
       </el-col>
-
-      <el-col :span="1" :offset="6" style="text-align: center;">
-        <p style="margin-top: 20px"><router-link to="/login">登录</router-link></p>
+      <el-col :span="2" :offset="4" style="text-align: center;" v-if="userStore.name === ''">
+        <p style="margin-top: 20px"><router-link to="/login">学生登陆</router-link></p>
       </el-col>
-      <el-col :span="1"  style="text-align: center;">
-        <p style="margin-top: 20px"><router-link to="/register">注册</router-link></p>
+      <el-col :span="2"  style="text-align: center;" v-if="userStore.name === ''">
+        <p style="margin-top: 20px"><router-link to="/login">专家登陆</router-link></p>
       </el-col>
-      <el-col :span="2"  style="text-align: center;">
-        <p style="margin-top: 20px"><router-link to="/login">管理入口</router-link></p>
+      <el-col :span="2"  style="text-align: center;" v-if="userStore.name === ''">
+        <p style="margin-top: 20px"><router-link to="/login">管理员入口</router-link></p>
+      </el-col>
+      <el-col :span="3" :offset="7" style="text-align: center;color:#1e90ff" v-if="userStore.name !== ''">
+        <p style="margin-top: 20px"><router-link to="/index">当前用户：{{ userStore.name }}</router-link></p>
       </el-col>
     </el-row>
     <router-view></router-view>
@@ -55,7 +57,10 @@
 </template>
 
 <script setup>
+import useUserStore from "@/store/modules/user";
 
+const userStore = useUserStore()
+console.log(userStore)
 </script>
 
 <style scoped>
